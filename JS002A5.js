0c43850498bf89b5d14c4a1befe0db8e73b39519
zavorszky <zavorszky@yahoo.com>
2018-02-04 20:26:55 +0100

    Biztonsági mentése

--- JS002A5.js
+++ JS002A5.js
@@ -11,47 +11,49 @@
  * B = C(onTimeOut)
  * D = C(onClick-StartStop)
  */
-function ido_kulonbseg_formazas(p_d1, p_d2) {
-  var v_osztok=[];
-  v_osztok[0]=['ms',1,0];
-  v_osztok[1]=['sec',1000,0];
-  v_osztok[2]=['min',60,0];
-  v_osztok[3]=['h',60,0];
-  v_osztok[4]=['d',24,0];
+
+function villogo_ini() {
+  v_osztok=[];
+  v_osztok[0]=['ms',1];
+  v_osztok[1]=['sec',1000];
+  v_osztok[2]=['min',60];
+  v_osztok[3]=['h',60];
+  v_osztok[4]=['d',24];
+  v_allapot="A";
+  v_timer_ref=undefined;
+  v_ido_start=undefined;
+} // villogo_ini
+
+function ido_kulonbseg_formazas(p_d2, p_d1) {
+  var v_maradekok=[0,0,0,0,0];
   
   var v_osztando, v_hanyados, v_maradek, i;
-  v_osztando=Math.ceil(Math.abs(p_d2.getTime() - p_d1.getTime())/1000);
+  v_osztando=Math.abs(p_d2.getTime() - p_d1.getTime());
   i=0;
   while ((v_osztando > 0) && (i < 4)) {
     i++;
-    v_hanyados=Math.ceil(v_osztando / v_osztok[i][1]);
+    v_hanyados=Math.floor(v_osztando / v_osztok[i][1]);
     v_maradek=v_osztando - (v_hanyados * v_osztok[i][1]);
-    v_osztok[(i-1)][2]=v_maradek;
+    v_maradekok[i-1]=v_maradek;
     v_osztando=v_hanyados;
   }
   
   //Formázás
   i=4;
-  while ((v_osztok[i][2] === 0) && (i > 1))
+  while ((v_maradekok[i] === 0) && (i > 1))
     i--;
   
   var v_txt="";
   while (i > 1) {
-    v_txt=v_txt + " " + v_osztok[i][2] + " " + v_osztok[i][0];
+    v_txt += " " + v_maradekok[i] + " " + v_osztok[i][0];
     i--;
   }
   
-  v_txt=v_txt + " " + v_osztok[i][2] + " " + v_osztok[i][0];
+  v_txt += " " + v_maradekok[i] + " " + v_osztok[i][0];
   
-  return v_txt.substring(1,100);
+  return v_txt.substring(1,1000);
   
 } //idokulonbseg_formazas
-
-function villogo_ini() {
-  v_allapot="A";
-  v_timer_ref=undefined;
-  v_ido_start=undefined;
-} // villogo_ini
 
 function villogo_valt(p_esemeny) {
   var v_allapot_uj=v_allapot;
